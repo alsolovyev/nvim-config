c75e2c6e024c6fbfa4f2ff093fc3f82a20b20877
Aleksey Solovyev <solovyev.a@icloud.com>
2024-03-23 17:18:50 +0300

    feat: add options

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 -- https://neovim.io/doc/user
 
-print('hi')
+require('options')
---
+++ lua/options.lua
@@ -0,0 +1,79 @@
+-- https://neovim.io/doc/user/options
+--
+-- :help option-list - list of options with short description
+-- :help E355 - list of options with full description
+--
+
+-- Common
+-- vim.o.inccommand = 'split'
+-- vim.o.list = true
+vim.o.listchars = 'eol:↲,extends:»,nbsp:␣,precedes:«,space:·,tab:→ ,trail:·'
+vim.o.scrolloff = 999
+-- vim.o.signcolumn = 'yes'
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.wrap = false
+
+-- Backup/Tmp Files
+vim.o.backup = false
+vim.o.swapfile = false
+vim.o.undofile = false
+vim.o.writebackup = false
+
+-- Highlight
+vim.o.cursorline = true
+vim.o.cursorlineopt = 'both'
+vim.o.termguicolors = true
+
+-- Cursor
+vim.o.guicursor = 'n-v-c-sm:block,i-ci-ve:hor20,r-cr-o:hor20'
+
+-- Title
+vim.o.title = true
+vim.o.titlestring = '%t'
+
+-- Search
+vim.o.ignorecase = true
+vim.o.showmatch = true
+vim.o.smartcase = true
+
+-- Tabs\Indent
+vim.o.autoindent = true
+vim.o.breakindent = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.smartindent = true
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+
+-- Clipboard
+vim.o.clipboard = 'unnamedplus'
+
+-- Numbers
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.ruler = false
+
+-- Others
+vim.o.updatetime = 250
+
+-- Files
+vim.opt.path:append({ '**' })
+vim.opt.wildignore:append({ '*/node_modules/*' })
+
+-- Plugins
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Disable default providers
+vim.g["loaded_node_provider"] = 0
+vim.g["loaded_python3_provider"] = 0
+vim.g["loaded_perl_provider"] = 0
+vim.g["loaded_ruby_provider"] = 0
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+vim.opt.whichwrap:append '<>[]hl'
+
+-- Disable nvim intro
+vim.opt.shortmess:append 'sI'
