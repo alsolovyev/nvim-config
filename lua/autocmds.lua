2d7878704b449371b5a296525dce8b1b86aecf5a
Aleksey Solovyev <solovyev.a@icloud.com>
2024-03-23 17:22:33 +0300

    feat: highlight when yanking (copying) text

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,3 +6,12 @@ vim.api.nvim_create_autocmd('VimLeave', {
   group = vim.api.nvim_create_augroup('RestoreCursor', { clear = true }),
   command = [[set guicursor=a:hor25]],
 })
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('HighlightYank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ timeout = 500 })
+  end
+})
