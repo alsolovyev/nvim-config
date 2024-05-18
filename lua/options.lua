7c02978397c8de48b0f9af45487ef2623e85105e
Aleksey Solovyev <solovyev.a@icloud.com>
2024-05-18 11:09:20 +0300

    feat: set colorcolumn to 80

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,7 @@
 --
 
 -- Common
+vim.opt.colorcolumn = '80'
 -- vim.o.inccommand = 'split'
 -- vim.o.list = true
 vim.o.listchars = 'eol:↲,extends:»,nbsp:␣,precedes:«,space:·,tab:→ ,trail:·'
@@ -86,6 +87,6 @@ vim.opt.shortmess:append 'sI'
 
 -- Add binaries installed by mason.nvim to path
 vim.env.PATH = vim.fn.stdpath 'data'
-  .. '/mason/bin'
-  .. (vim.fn.has 'win32' ~= 0 and ';' or ':')
-  .. vim.env.PATH
+    .. '/mason/bin'
+    .. (vim.fn.has 'win32' ~= 0 and ';' or ':')
+    .. vim.env.PATH
