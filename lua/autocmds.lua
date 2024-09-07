14b92e4b49051f87f008b9069c6a588f8cb77412
Aleksey Solovyev <solovyev.a@icloud.com>
2024-09-07 09:03:23 +0300

    refactor(vim): update logic to organize Go imports on BufWritePre
    Fixed an issue where the previous logic removed necessary
    imports or caused incorrect organization

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -7,28 +7,36 @@ vim.api.nvim_create_autocmd('VimLeave', {
   command = [[set guicursor=a:hor25]],
 })
 
--- Automatically organize imports before writing buffer contents for Go files
-vim.api.nvim_create_autocmd('BufWritePre', {
-  desc = 'Organize imports before saving buffer (Go files)',
-  group = vim.api.nvim_create_augroup('OrganizeImports', { clear = true }),
-  pattern = '*.go',
-  callback = function()
-    if vim.bo.modified then
-      vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
-    end
-  end
-})
-
 -- Automatically trigger code formatting before writing buffer contents
 vim.api.nvim_create_autocmd('BufWritePre', {
   desc = 'Automatically format code before saving buffer',
   group = vim.api.nvim_create_augroup('CodeFormatting', { clear = true }),
   callback = function()
-    if not vim.bo.modified or not vim.bo.modifiable or vim.bo.binary then
-      return
+    if vim.bo.modified or vim.bo.modifiable or not vim.bo.binary then
+      vim.lsp.buf.format({ async = false })
     end
+  end
+})
 
-    vim.lsp.buf.format()
+-- Automatically organize imports before writing buffer contents for Go files
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+vim.api.nvim_create_autocmd('BufWritePre', {
+  desc = 'Organize imports before saving buffer (Go files)',
+  group = vim.api.nvim_create_augroup('OrganizeImports', { clear = true }),
+  pattern = '*.go',
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
   end
 })
 
