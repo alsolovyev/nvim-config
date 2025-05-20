be9c9c9ae67bd4123bdcdae261813959f5dc9a66
Aleksey Solovyev <solovyev.a@icloud.com>
2025-05-20 18:46:32 +0300

    fix: replace deprecated vim.fn.sign_define fn with vim.diagnostic.config

--- lua/signs.lua
+++ lua/signs.lua
@@ -1,16 +1,14 @@
--- https://neovim.io/doc/user/sign.html#%3Asign-place
 --
 -- :help signs
 --
 
---- Define a new sign named or modify the attributes of an existing sign.
-local function sign_define(hl, icon)
-  -- https://neovim.io/doc/user/builtin.html#sign_define()
-  vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-end
-
--- LSP
-sign_define('DiagnosticSignError', '󰅙')
-sign_define('DiagnosticSignInfo',  '󰋼')
-sign_define('DiagnosticSignHint',  '󰌵')
-sign_define('DiagnosticSignWarn',  '')
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅙',
+      [vim.diagnostic.severity.HINT] = '󰌵',
+      [vim.diagnostic.severity.INFO] = '󰋼',
+      [vim.diagnostic.severity.WARN] = '',
+    }
+  }
+})
