631f559012fc3606e9dfe149ae8422164dea6be6
Aleksey Solovyev <solovyev.a@icloud.com>
2025-05-21 09:54:20 +0300

    feat: delete lsp server configurations

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -22,32 +22,6 @@ M.config = function()
       }
     }
   })
-
-  -- Server configurations
-  local lspconfig = require('lspconfig')
-
-  lspconfig.lua_ls.setup({
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { 'vim' },
-        },
-        workspace = {
-          library = {
-            [vim.fn.expand '$VIMRUNTIME/lua'] = true,
-            [vim.fn.expand '$VIMRUNTIME/lua/vim/lsp'] = true,
-            [vim.fn.stdpath 'data' .. '/lazy/lazy.nvim/lua/lazy'] = true,
-          },
-          maxPreload = 100000,
-          preloadFileSize = 10000,
-        },
-      },
-    },
-  })
-
-  lspconfig.gopls.setup {}
-  lspconfig.ruff.setup {}
-  lspconfig.ts_ls.setup {}
 end
 
 return M
