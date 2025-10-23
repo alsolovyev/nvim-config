3d76a07c4e387a0fb3c2f96ee08ffbdfa23127d4
Aleksey Solovyev <solovyev.a@icloud.com>
2025-10-23 23:20:36 +0300

    feat: change event trigger from WinScrolled to User Filepost

--- lua/plugins/configs/treesitter-context.lua
+++ lua/plugins/configs/treesitter-context.lua
@@ -1,7 +1,7 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter-context
 local M = { 'nvim-treesitter/nvim-treesitter-context' }
 
-M.event = 'WinScrolled'
+M.event = 'User Filepost' 
 M.dependencies = { 'nvim-treesitter/nvim-treesitter' }
 M.opts = {
   max_lines = 1
