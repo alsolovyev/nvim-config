5c075bb50dfa06e93a7aa890ed4f0b767cd99b66
Aleksey Solovyev <solovyev.a@icloud.com>
2026-02-06 21:36:12 +0300

    feat: enable filesystem watcher in nvim tree

--- lua/plugins/configs/nvim-tree.lua
+++ lua/plugins/configs/nvim-tree.lua
@@ -6,9 +6,6 @@ M.dependencies = { 'nvim-tree/nvim-web-devicons' }
 M.opts = {
   -- https://github.com/nvim-tree/nvim-tree.lua/blob/master/doc/nvim-tree-lua.txt
   disable_netrw = true,
-  filesystem_watchers = {
-    enable = false
-  },
   view = {
     width = 20,
   },
