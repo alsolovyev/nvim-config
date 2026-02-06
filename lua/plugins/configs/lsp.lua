4e597f3fc871231e871532946d5d59575128f8ad
Aleksey Solovyev <solovyev.a@icloud.com>
2026-02-07 01:56:55 +0300

    feat: add vue support

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -25,7 +25,7 @@ M.config = function()
   -- Common
   vim.lsp.config('*', {
     capabilities = vim.lsp.protocol.make_client_capabilities(),
-    root_markers = { '.git' },
+    -- root_markers = { '.git' },
     flags = {
       debounce_text_changes = 500,
     },
@@ -42,12 +42,43 @@ M.config = function()
   -- Golang
   vim.lsp.enable('gopls')
 
+  -- Vue
+  vim.lsp.config('vtsls', {
+    settings = {
+      vtsls = {
+        tsserver = {
+          globalPlugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+              languages = { 'vue' },
+              configNamespace = 'typescript',
+            },
+          },
+        },
+      },
+    },
+    filetypes = { 'vue' }
+  })
+  vim.lsp.enable("vtsls")
+
+  vim.lsp.config('vue_ls', {
+    settings = {
+      init_options = {
+        typescript = {
+          tsdk = '',
+        },
+      },
+    },
+  })
+  vim.lsp.enable("vue_ls")
+
   if require("lspconfig").util.root_pattern("deno.json")(0) then
     -- Deno
     vim.lsp.enable('denols')
   else
     -- TypeScript
-    vim.lsp.enable('ts_ls') -- TypeScript
+    vim.lsp.enable('ts_ls')
   end
 end
 
